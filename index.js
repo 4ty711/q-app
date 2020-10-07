7af08b02a4a8d83af60a360b6f398a352d40cd4a
Marco Boelling <53232140+4ty711@users.noreply.github.com>
2020-10-07 17:01:38 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,143 @@
+var kju = new KJU();
+
+var app = new Vue({
+    el: '#app',
+    watch: {
+        newMessageShown(val) {
+            console.log(this.$refs)
+            if (val) document.getElementById('contentInput').focus();
+        }
+    },
+    data: () => {
+        return {
+            me: null,
+            showMe: false,
+            creationToken: null,
+            showCreationToken: false,
+            newMessageResponse: "",
+            newMessage: {
+                content: "",
+                reciever: "",
+                responses: []
+            },
+            newMessageShown: false,
+            tags: ["boelling", "spoo"],
+            messages: [
+            ],
+            participants: [{
+                    name: "benjamin.lotterer@spoo-group.com",
+                    short: "BL"
+                },
+                {
+                    name: "moritz.roessler@spoo-group.com",
+                    short: "MR"
+                }
+            ]
+        }
+    },
+    methods: {
+        // API Functions
+        createToken() {
+
+            if (this.creationToken) {
+                this.showCreationToken = !this.showCreationToken;
+                return;
+            }
+
+            var me = prompt('Enter your email. Your token will be sent to you');
+            if (!me) return;
+
+            kju.createToken(token => {
+                localStorage['creationToken'] = token;
+                this.creationToken = token;
+            })
+        },
+
+        createMessage(msg) {
+            kju.createMessage({
+                msg: {
+                    content: msg.content,
+                    reciever: msg.reciever,
+                    responses: msg.responses
+                },
+                token: this.creationToken
+            }, data => {
+
+                data.mine = true;
+
+                fs.writeFile('/q/' + data._id, JSON.stringify(data), {}, (err, d) => {
+                    if (!err) {
+                        this.messages.unshift(data)
+                    }
+                })
+
+            })
+        },
+
+        deleteMessage(msgId) {
+            if(!confirm('Really delete?')) return;
+
+            kj.r
+        },
+
+        // Frontend Functions
+        deleteCreationToken() {
+            this.creationToken = null;
+            delete localStorage['creationToken'];
+            this.showCreationToken = false;
+        },
+        saveCreationToken(token) {
+            this.creationToken = token;
+            localStorage['creationToken'] = token;
+        },
+        deleteMe() {
+            this.me = null;
+            delete localStorage['me'];
+            this.showMe = false;
+        },
+        setMe() {
+            var me = prompt('enter email')
+            this.me = me;
+            localStorage['me'] = me;
+            this.showMe = true;
+        },
+        addRecieverToNewMessage(evt) {
+            this.newMessage.responses.push({ title: evt.target.value })
+            this.newMessageResponse = null;
+        },
+        removeResponseFromNewMessage(title) {
+            this.newMessage.responses.forEach((res, i) => {
+                if (res.title == title) this.newMessage.responses.splice(i, 1);
+            })
+        },
+        popResponseFromNewMessage(evt) {
+            if (evt.target.value.length == 0) this.newMessage.responses.pop()
+        }
+    },
+    created() {
+        if (localStorage['creationToken']) Vue.set(this, 'creationToken', localStorage['creationToken']);
+        if (localStorage['me']) Vue.set(this, 'me', localStorage['me']);
+
+        window.addEventListener('DOMContentLoaded', (event) => {
+
+            fs.mkdir('/q', {}, (err, data) => {
+                if (!err) {
+                    console.log(data)
+                }
+            })
+
+            fs.readdir('/q', {}, (err, data) => {
+                if (!err) {
+
+                    data.forEach(d => {
+                        fs.readFile('/q/' + d, { 'encoding': 'utf8' }, (err, file) => {
+                            if (!err) this.messages.unshift(JSON.parse(file))
+                        })
+
+                    })
+                }
+            })
+
+        });
+    }
+})
